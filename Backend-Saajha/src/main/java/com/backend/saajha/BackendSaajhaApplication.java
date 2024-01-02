46078b41312e9600b953ccaa75772d0c95f2a439
sonigitkhushi <ks3141591@gmail.com>
2024-01-02 16:17:21 +0530

    saajha commit

---
+++ Backend-Saajha/src/main/java/com/backend/saajha/BackendSaajhaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.saajha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSaajhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSaajhaApplication.class, args);
+	}
+
+}
---
+++ Backend-Saajha/src/test/java/com/backend/saajha/BackendSaajhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.saajha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSaajhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
